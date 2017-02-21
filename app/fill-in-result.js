c43fae3e745598ca26faf8ae5c0570220b4690d3
Will Pacetti <Slade@Will-Pacettis-MacBook-Pro.local>
2017-02-21 14:00:37 -0600

    Changed image method to .src

--- app/fill-in-result.js
+++ app/fill-in-result.js
@@ -2,8 +2,9 @@ export default function fillInResult(element, data) {
   element.querySelector('.movie-title').innerText = data.show_title;
   element.querySelector('.movie-genre').innerText = data.category;
   element.querySelector('.movie-score').innerText = data.rating;
-  element.querySelector('.movie-profile__img').innerText = data.poster;
+  element.querySelector('.movie-profile__img').src = data.poster;  // innerHTML ?  (is the image considered text here?)
   element.querySelector('.movie-summary__text').innerText = data.summary;
   element.querySelector('.movie-actors__text').innerText = data.show_cast;
+
   return element;
 }
--- app/index.js
+++ app/index.js
@@ -1,17 +1,24 @@
 import 'whatwg-fetch';
 import fillInResult from './fill-in-result';
 
-const searchForm = document.querySelector('.search-submit');
-const searchInput = document.querySelector('.search');
+const searchForm = document.querySelector('.search-submit'); // not sure where to use these
+const searchInput = document.querySelector('.search');  // not sure where to use these
 const searchResult = document.querySelector('.bottom');
 
-fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
-  .then((res) => res.json())
-  .then((data) => {
-    fillInResult(searchResult, data); //  not sure about quotes inside of parentheses
-  });
+// Not sure about the wrapping a function around the fetch here.
+// Why would it be necessary? Does it not call otherwise?
+function searchForShow(name) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${name}`)
+    .then((res) => res.json())
+    .then((data) => {
+      fillInResult(searchResult, data);
+    });
+}
 
+searchForShow('Attack On Titan'); // argument is unnecessary here since the fetch fetches data for Attack on Titan no matter what right?
 
-// searchForm.addEventListener('click', () => {
-//    fillInResult(searchInput.value);
-// });
+  // do I need the %20 to represent spaces?
+searchForm.addEventListener('click', () => {
+
+  return searchForShow(searchInput.value);
+});
