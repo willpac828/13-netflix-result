2d16ab6f3bc24cef813c07281d16c7fe11683ee1
Will Pacetti <Slade@Will-Pacettis-MacBook-Pro.local>
2017-04-02 17:06:56 -0500

    Added star icon

--- app/fill-in-result.js
+++ app/fill-in-result.js
@@ -1,7 +1,8 @@
 export default function fillInResult(element, data) {
+  // filling in the elements inside of .bottom with the data api
   element.querySelector('.movie-title').innerText = data.show_title;
   element.querySelector('.movie-genre').innerText = data.category;
-  element.querySelector('.movie-score').innerText = data.rating;
+  element.querySelector('.movie-score').innerHTML = `<p> ${data.rating} <i class="fa fa-star" aria-hidden="true"></i></p>`;
   element.querySelector('.movie-profile__img').src = data.poster;  // innerHTML ?  (is the image considered text here?)
   element.querySelector('.movie-summary__text').innerText = data.summary;
   element.querySelector('.movie-actors__text').innerText = data.show_cast;
--- app/index.js
+++ app/index.js
@@ -1,24 +1,31 @@
 import 'whatwg-fetch';
 import fillInResult from './fill-in-result';
 
-const searchForm = document.querySelector('.search-submit'); // not sure where to use these
-const searchInput = document.querySelector('.search');  // not sure where to use these
+// Are these the starting values??
+const searchForm = document.querySelector('.search-submit');
+const searchInput = document.querySelector('.search');
 const searchResult = document.querySelector('.bottom');
 
 // Not sure about the wrapping a function around the fetch here.
 // Why would it be necessary? Does it not call otherwise?
 function searchForShow(name) {
   fetch(`http://netflixroulette.net/api/api.php?title=${name}`)
+    // once I have data, turn it into a javascript object and perform work
     .then((res) => res.json())
     .then((data) => {
+      // I defined searchResult to represent .bottom
       fillInResult(searchResult, data);
     });
 }
 
-searchForShow('Attack On Titan'); // argument is unnecessary here since the fetch fetches data for Attack on Titan no matter what right?
+searchForShow('Attack On Titan');
+
+// argument is unnecessary here since the fetch fetches data for Attack on Titan no matter what right?
 
   // do I need the %20 to represent spaces?
 searchForm.addEventListener('click', () => {
 
-  return searchForShow(searchInput.value);
+  return searchForShow(searchInput.value); //searchInput.value finds whatever searchInput/.search contains at present
 });
+
+// when button is clicked, function runs for whatever value is typed
